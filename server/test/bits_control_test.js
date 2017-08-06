3fd53d10212c2eba715ad22c599ab866b191b08a
Jimmy Nugent <jimmyfnugent@gmail.com>
2017-08-06 03:03:23 -0400

    Fixed indentation

--- server/test/bits_control_test.js
+++ server/test/bits_control_test.js
@@ -4,53 +4,53 @@ import {Map, fromJS} from 'immutable';
 import {addBit} from '../src/bits_control';
 
 describe('bits control logic', () => {
-	describe('add bit', () => {
- 		it('adds a bit to an empty initial state', () => {
- 			const bit = fromJS({
- 				id: 'L3WDO8EL3LEKS',
- 				author: {
- 					person_id: 'I2L3KFAE9GLREJ3',
- 				},
- 				date_created: new Date(2017, 5, 1, 12, 12),
- 				upvotes: 10,
- 				downvotes: 3,
- 				title: 'Fox is unedgeguardable',
- 				content: 'No matter what you do, you\'ll never be able to kill a recovering Fox.',
- 			});
- 			const newState = addBit(Map(), bit);
- 			expect(newState).to.equal(fromJS({bits: [bit]}));
- 		});
+  describe('add bit', () => {
+    it('adds a bit to an empty initial state', () => {
+      const bit = fromJS({
+        id: 'L3WDO8EL3LEKS',
+        author: {
+          person_id: 'I2L3KFAE9GLREJ3',
+        },
+        date_created: new Date(2017, 5, 1, 12, 12),
+        upvotes: 10,
+        downvotes: 3,
+        title: 'Fox is unedgeguardable',
+        content: 'No matter what you do, you\'ll never be able to kill a recovering Fox.',
+      });
+      const newState = addBit(Map(), bit);
+      expect(newState).to.equal(fromJS({bits: [bit]}));
+    });
 
- 		it('adds a bit to a state with existing bits', () => {
- 			const initialBit = fromJS({
-	 				id: 'ME8DU23MNO0S',
-	 				author: {
-	 					person_id: '562B3409SLL',
-	 				},
-	 				date_created: new Date(1993, 6, 24),
-	 				upvotes: 42,
-	 				downvotes: 8,
-	 				title: 'Master Hand\'s getup attack',
-	 				content: 'It\'s a 1HKO.',
-	 			});
+    it('adds a bit to a state with existing bits', () => {
+      const initialBit = fromJS({
+          id: 'ME8DU23MNO0S',
+          author: {
+            person_id: '562B3409SLL',
+          },
+          date_created: new Date(1993, 6, 24),
+          upvotes: 42,
+          downvotes: 8,
+          title: 'Master Hand\'s getup attack',
+          content: 'It\'s a 1HKO.',
+        });
 
- 			const initialState = fromJS({
- 				bits: [initialBit],
- 			});
+      const initialState = fromJS({
+        bits: [initialBit],
+      });
 
- 			const newBit = fromJS({
- 				id: 'L3WDO8EL3LEKS',
- 				author: {
- 					person_id: 'I2L3KFAE9GLREJ3',
- 				},
- 				date_created: new Date(2017, 5, 1, 12, 12),
- 				upvotes: 10,
- 				downvotes: 3,
- 				title: 'Fox is unedgeguardable',
- 				content: 'No matter what you do, you\'ll never be able to kill a recovering Fox.',
- 			});
- 			const newState = addBit(initialState, newBit);
- 			expect(newState).to.equal(fromJS({bits: [initialBit, newBit]}));
- 		});
-	});
+      const newBit = fromJS({
+        id: 'L3WDO8EL3LEKS',
+        author: {
+          person_id: 'I2L3KFAE9GLREJ3',
+        },
+        date_created: new Date(2017, 5, 1, 12, 12),
+        upvotes: 10,
+        downvotes: 3,
+        title: 'Fox is unedgeguardable',
+        content: 'No matter what you do, you\'ll never be able to kill a recovering Fox.',
+      });
+      const newState = addBit(initialState, newBit);
+      expect(newState).to.equal(fromJS({bits: [initialBit, newBit]}));
+    });
+  });
 });
