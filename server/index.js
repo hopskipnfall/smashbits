a990daa3009f64280b11d649f1b5be27fdad045e
Jimmy Nugent <jimmyfnugent@gmail.com>
2018-04-07 23:55:33 -0400

    Add API to create bits.

--- server/index.js
+++ server/index.js
@@ -1,16 +1,27 @@
 import serverless from 'serverless-http';
 import express from 'express';
-import getBits from './src/store';
+import helmet from 'helmet';
+import { getBits, createBit } from './src/store';
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json()); // support encoded JSON
+app.use(express.urlencoded({ extended: true })); // support encoded bodies
+// Security middlewhere, but keep client-side caching.
+app.use(helmet({
+  noCache: {
+    action: 'deny'
+  }
+}));
 
 app.get('/bits', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(getBits()));
 });
+
+app.post('/bits', (req, res) => {
+  createBit(req.body.bit)
+      .then(result => res.location('/bits/' + result.postId).sendStatus(201))
+      .catch(error => res.status(500).send(error));
+})
 
 export const handler = serverless(app);
--- server/src/store.js
+++ server/src/store.js
@@ -1,3 +1,7 @@
+import uuid from "uuid";
+import AWS from "aws-sdk";
+import jsStringEscape from 'js-string-escape';
+
 // TODO(thenuge): Replace this test data with real data from the DB.
 const foxBit = {
     id: 'L3WDO8EL3LEKS',
@@ -43,6 +47,37 @@ const falconPressureBit = {
 };
 const bits = {bits: [foxBit, handBit, falconPressureBit]};
 
-export default function getBits() {
+AWS.config.update({ region: "us-east-2" });
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export function getBits() {
   return bits;
+}
+
+export function createBit(bit) {
+  const params = {
+      TableName: 'posts',
+      Item: {
+        postId: uuid.v1(),
+        author: {
+          name: jsStringEscape(bit.author.name),
+          personId: jsStringEscape(bit.author.person_id)
+        },
+        dateCreated: new Date().getTime(),
+        upvotes: 0,
+        downvotes: 0,
+        title: jsStringEscape(bit.title),
+        content: jsStringEscape(bit.content),
+        ...(bit.tags ? { tags: jsStringEscape(bit.tags.join()) } : {}),
+        ...(bit.stages ? { stages: jsStringEscape(bit.stages.join()) } : {}),
+        ...(bit.mainChars ? { main_chars: jsStringEscape(bit.main_chars.join()) } : {}),
+        ...(bit.vsChars ? { vs_chars: jsStringEscape(bit.vsCharss.join()) } : {})
+      }
+    };
+
+  return new Promise((resolve, reject) => {
+    dynamoDb.put(params).promise()
+        .then(data => resolve(params.Item))
+        .catch(err => reject(err));
+  });
 }
