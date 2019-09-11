e85323e60208cef90de1ef94473ae35e11f0294f
Jimmy Nugent <jimmyfnugent@gmail.com>
2019-09-11 00:07:52 -0400

    Cleanup

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -2,6 +2,7 @@ const path = require('path');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 
 module.exports = function override(config, env) {
+    // Remove the create-react-app plugin that limits source files to the src/ directory.
     config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
 
     config.resolve.alias = {
@@ -10,14 +11,3 @@ module.exports = function override(config, env) {
 
     return config;
 };
-
-/*module.exports = {
-    paths: function (paths, env) {
-        paths.appIndexJs = path.resolve(__dirname, 'src/index.js');
-        paths.appSrc = [
-          path.resolve(__dirname, 'src'),
-          path.resolve('../shared/src'),
-        ];
-        return paths;
-    },
-}*/
