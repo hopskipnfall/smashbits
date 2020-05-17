bdbbb78248fc0363db16b083f6916616cc96fc21
Jimmy Nugent <jimmyfnugent@gmail.com>
2020-05-17 15:02:29 -0400

    More cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,29 @@
-import React, { Component } from "react";
-import "./App.css";
-import BitPage from "./BitPage";
-import Home from "./Home";
-import { Route, Switch } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import React, { Component } from 'react';
+import './App.css';
+import BitPage from './BitPage';
+import Home from './Home';
+import { Route, Switch } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div className="App">
-          <div className="App-header">
+        <div className='App'>
+          <div className='App-header'>
             <h2>Welcome to SmashBits!</h2>
-            <a href="http://localhost:3001/login/twitter">
+            <a href='http://localhost:3001/login/twitter'>
               <Button>Log in with Twitter</Button>
             </a>
           </div>
-          <p className="App-intro">
+          <p className='App-intro'>
             SmashBits is a place to collaborate on and organize game knowledge
             in a central, public place.
           </p>
         </div>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/bits/:bitId" component={BitPage} />
+          <Route exact path='/' component={Home} />
+          <Route path='/bits/:bitId' component={BitPage} />
         </Switch>
       </div>
     );
