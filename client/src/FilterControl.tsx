4797aff5e92ee29c4bf8871c6af7591a65490741
Jonn Spencer <jonn@jonnjonnjonn.com>
2020-07-04 21:24:36 +0900

    Filter stuff done?

--- client/src/FilterControl.tsx
+++ client/src/FilterControl.tsx
@@ -1,52 +1,62 @@
-// import { fromJS, List, Map } from 'immutable';
-// import * as React from 'react';
-// import { Card } from 'react-bootstrap';
-// import { connect } from 'react-redux';
-// import * as actionCreators from './action_creators';
-// import FilterMenu from './FilterMenu';
+import { fromJS, List, Map } from 'immutable';
+import * as React from 'react';
+import { Card } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import FilterMenu from './FilterMenu';
+import { allActions } from './all_actions';
+import { AppFunctionComponent, NOOP, AppState } from './store';
+import { STAGE_MAP, CHARACTER_MAP, LABEL_MAP } from './types';
 
-// const FilterControl = (props: any) => {
-//   const {
-//     filtering = Map(), toggleMainCharFilter, toggleVsCharFilter, toggleStageFilter, toggleStandaloneTagFilter,
-//   } = props;
-//   return (
-//     <Card>
-//       <Card.Header><h2> Filtering Options </h2></Card.Header>
-//       <h4> Show me bits about: </h4>
-//       <FilterMenu
-//         title="These characters"
-//         bootstrapStyle="success"
-//         allFilters={filtering.get('chars')}
-//         currentFilters={filtering.get('currentMainChars', List())}
-//         onClick={toggleMainCharFilter}
-//       />
-//       <FilterMenu
-//         title="vs. these characters"
-//         bootstrapStyle="danger"
-//         allFilters={filtering.get('chars')}
-//         currentFilters={filtering.get('currentVsChars', List())}
-//         onClick={toggleVsCharFilter}
-//       />
-//       <FilterMenu
-//         title="on these stages"
-//         bootstrapStyle="primary"
-//         allFilters={filtering.get('stages')}
-//         currentFilters={filtering.get('currentStages', List())}
-//         onClick={toggleStageFilter}
-//       />
-//       <FilterMenu
-//         title="with these tags"
-//         bootstrapStyle="warning"
-//         allFilters={filtering.get('standaloneTags')}
-//         currentFilters={filtering.get('currentStandaloneTags', List())}
-//         onClick={toggleStandaloneTagFilter}
-//       />
-//     </Card>
-//   );
-// };
+type Parameters = {};
 
-// const mapStateToProps = (state: Map<string, any>, ownProps: any) => ({
-//   filtering: state.get('filtering').merge(fromJS(ownProps.filters)),
-// });
+const mapStateToProps = (state: AppState, ownProps: any) => ({
+  filtering: state.filtering,
+  // filtering: state.get('filtering').merge(fromJS(ownProps.filters)),
+});
 
-// export default connect(mapStateToProps, actionCreators)(FilterControl);
+const FilterControl: AppFunctionComponent<Parameters, typeof mapStateToProps> = props => {
+  const {
+    filtering
+    //  toggleMainCharFilter, toggleVsCharFilter, toggleStageFilter, toggleStandaloneTagFilter,
+  } = props;
+  return (
+    <Card>
+      <Card.Header><h2> Filtering Options </h2></Card.Header>
+      <h4> Show me bits about: </h4>
+      <FilterMenu
+        title="These characters"
+        bootstrapStyle="success"
+        allFilters={Array.from(CHARACTER_MAP).map(r=> r[1].display)}
+        currentFilters={filtering.mainCharacters}
+        onClick={() => console.log("do toggleMainCharFilter!")}
+        // onClick={toggleMainCharFilter}
+      />
+      <FilterMenu
+        title="vs. these characters"
+        bootstrapStyle="danger"
+        allFilters={Array.from(CHARACTER_MAP).map(r=> r[1].display)}
+        currentFilters={filtering.vsCharacters}
+        onClick={() => console.log("do toggleVsCharFilter!")}
+        // onClick={toggleVsCharFilter}
+      />
+      <FilterMenu
+        title="on these stages"
+        bootstrapStyle="primary"
+        allFilters={Array.from(STAGE_MAP).map(r=> r[1].display)}
+        currentFilters={filtering.stages}
+        onClick={() => console.log("do toggleStageFilter!")}
+        // onClick={toggleStageFilter}
+      />
+      <FilterMenu
+        title="with these tags"
+        bootstrapStyle="warning"
+        allFilters={Array.from(LABEL_MAP).map(r=> r[1].display)}
+        currentFilters={filtering.labels}
+        onClick={() => console.log("do toggleStandaloneTagFilter!")}
+        // onClick={toggleStandaloneTagFilter}
+      />
+    </Card>
+  );
+};
+
+export default connect(mapStateToProps, allActions)(FilterControl);
--- client/src/FilterMenu.tsx
+++ client/src/FilterMenu.tsx
@@ -1,43 +1,48 @@
-// import * as React from 'react';
-// import { Badge, Dropdown } from 'react-bootstrap';
+import * as React from 'react';
+import { Badge, Dropdown } from 'react-bootstrap';
+import { AppFunctionComponent, NOOP } from './store';
+import { connect } from 'react-redux';
+import { allActions } from './all_actions';
 
-// type Props = {
-//   title: any
-//   bootstrapStyle: any
-//   allFilters: any
-//   currentFilters: any
-//   onClick: (filter: string) => any
-// };
+type Parameters = {
+  title: any
+  bootstrapStyle: any
+  allFilters: any
+  currentFilters: any
+  onClick: (filter: string) => any
+};
 
-// export default function render(props: Props) {
-//   const {
-//     title, bootstrapStyle, allFilters, currentFilters, onClick,
-//   } = props;
-//   return (
-//     <div className="panel-body">
-//       <Dropdown className="dropdown">
-//         <Dropdown.Toggle variant={bootstrapStyle} id="dropdown-basic">
-//           {title}
-//         </Dropdown.Toggle>
+const FilterMenu: AppFunctionComponent<Parameters, NOOP> = props => {
+  const {
+    title, bootstrapStyle, allFilters, currentFilters, onClick,
+  } = props;
+  return (
+    <div className="panel-body">
+      <Dropdown className="dropdown">
+        <Dropdown.Toggle variant={bootstrapStyle} id="dropdown-basic">
+          {title}
+        </Dropdown.Toggle>
 
-//         <Dropdown.Menu className="dropdown-menu">
-//           {allFilters.map((filter: string) => (
-//             <Dropdown.Item onSelect={() => onClick(filter)} key={filter}>
-//               {(currentFilters.includes(filter) ? '\u2713 ' : ' ') + filter}
-//             </Dropdown.Item>
-//           ))}
-//         </Dropdown.Menu>
-//       </Dropdown>
+        <Dropdown.Menu className="dropdown-menu">
+          {allFilters.map((filter: string) => (
+            <Dropdown.Item onSelect={() => onClick(filter)} key={filter}>
+              {(currentFilters.includes(filter) ? '\u2713 ' : ' ') + filter}
+            </Dropdown.Item>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
 
-//       <div>
-//         {currentFilters.map((filter: string) => (
-//           <Badge variant={bootstrapStyle} className="filter-pill" onClick={() => onClick(filter)} key={filter}>
-//             &#215;
-//             {' '}
-//             {filter}
-//           </Badge>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
+      <div>
+        {currentFilters.map((filter: string) => (
+          <Badge variant={bootstrapStyle} className="filter-pill" onClick={() => onClick(filter)} key={filter}>
+            &#215;
+            {' '}
+            {filter}
+          </Badge>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default connect(null, allActions)(FilterMenu);
