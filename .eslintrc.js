98513c1c8a5ff97239d4488aefa9e76b026fa36b
Jonn Spencer <jonn@jonnjonnjonn.com>
2020-10-15 19:05:30 +0900

    Combine everything.

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    es2017: true,
+  },
+  extends: [
+    'airbnb-typescript',
+    'prettier', // Prettier modules must go last.
+    'prettier/unicorn',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    project: [
+      './tsconfig.base.json',
+      './client/tsconfig.json',
+      './server/tsconfig.json',
+    ],
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  plugins: ['@typescript-eslint', 'unicorn', 'prettier'],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'no-unused-vars': 'off', // Broken for type imports...
+  },
+  ignorePatterns: [
+    'client/node_modules',
+    'client/.build',
+    'client/dist',
+    'server/node_modules',
+    'server/.build',
+    'server/dist',
+    // 'src/**/*.js',
+    // 'main.js',
+  ],
+};
---
+++ shared/README.md
@@ -0,0 +1,3 @@
+## shared/
+
+Files in this directory are visible from both `../server/` and `../client/`.
